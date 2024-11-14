5bd22bf1fa77a535e9080b9459d6bb9c20c14687
Alexeyodu <oducentralexey@gmail.com>
2024-11-14 13:22:34 +0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,6 @@
+@router.logout("/logout")
+async def logout(
+      response: Response,   
+):
+                  response.delete_cookie(key="access_token")
+                  return {"status":"ok"}     
